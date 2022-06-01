0cb1c7f4721b31c785ba1c46cbc7c99db3914218
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-06-01 11:57:55 +0900

    [uodate]アプリ名変更

--- associationApp/wsgi.py
+++ associationApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for loginApp project.
+WSGI config for associationApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loginApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'associationApp.settings')
 
 application = get_wsgi_application()
