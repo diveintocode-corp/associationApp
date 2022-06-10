c901f40a4f1dc2d6911244a823f788c55f913cdd
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2022-06-10 11:45:50 +0900

    Update models.py

--- books/models.py
+++ books/models.py
@@ -3,10 +3,9 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField()
 
     class Meta:
         db_table = 'books'
 
     def __str__(self):
-        return f'{self.title}, {self.content}'
+        return f'{self.title}'
